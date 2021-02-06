80e83ade42c7c6aa49a6c1d39cbf3a5964f4034f
carlosiglesias1 <carlosig490@gmail.com>
2021-02-06 03:54:08 +0100

    commit on master

--- probas/src/test/java/proyecto_division/DivisionTest.java
+++ probas/src/test/java/proyecto_division/DivisionTest.java
@@ -3,9 +3,12 @@ package proyecto_division;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 public class DivisionTest {
 
@@ -31,8 +34,8 @@ public class DivisionTest {
     }
 
     @Test
-    public void testCalcularDivision() throws Exception {
-        System.out.println("calcularDivision");
+    public void testDivisionPositiva() throws Exception {
+        System.out.println("calcularDivisionPositivos");
         float dividendo = 10.0F;
         float divisor = 5.0F;
         Division instance = new Division();
@@ -40,5 +43,53 @@ public class DivisionTest {
         float result = instance.calcularDivision(dividendo, divisor);
         assertEquals(expResult, result, 0.0);
     }
+    
+    @Test
+    public void testDivisionNegativa () throws Exception {
+        System.out.println("calcularDivisionNegativa");
+        float dividendo = 10.0F;
+        float divisor = -5.0F;
+        Division instance = new Division();
+        float expResult = -2.0F;
+        float result = instance.calcularDivision(dividendo, divisor);
+        assertEquals(expResult, result, 0.0);
+    }
+    
+    @Test
+    public void testDivisionNula () throws Exception {
+        System.out.println("calcularDivisionNula");
+        Division instance = new Division();
+        assertThrows(Exception.class, new Executable(){
+            @Override
+            public void execute () throws Throwable{
+                instance.calcularDivision(5, 0);
+            }
+        });
+    }
+
+    @Test
+    public void testDivisionCaracter () throws Exception {
+        System.out.println("calcularDivisionCaracter");
+        Division instance = new Division();
+        assertThrows(Exception.class, new Executable(){
+            @Override
+            public void execute () throws Throwable{
+                instance.calcularDivision('a', 1);
+            }
+        });
+    }
+
+    @Test
+    public void testDivisorCaracter () throws Exception {
+        System.out.println("calcularDivisorCaracter");
+        Division instance = new Division();
+        assertThrows(Exception.class, new Executable(){
+            @Override
+            public void execute () throws Throwable{
+                instance.calcularDivision(5, 'b');
+            }
+        });
+    }
+
 
 }
